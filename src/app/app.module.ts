6fda732413ab22f545d3df150624c14e6912a591
Davids148 <david.stojanovski@yahoo.com>
2018-03-13 13:04:12 +0100

    Commit one

---
+++ src/app/app.module.ts
@@ -0,0 +1,41 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+import { ContactPage } from '../pages/contact/contact';
+import { HomePage } from '../pages/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import {MissingStickersPage} from "../pages/missingstickers/missingstickers";
+
+@NgModule({
+  declarations: [
+    MyApp,
+    MissingStickersPage,
+    ContactPage,
+    HomePage,
+    TabsPage
+
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    MissingStickersPage,
+    ContactPage,
+    HomePage,
+    TabsPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ src/pages/missingstickers/missingstickers.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-about',
+  templateUrl: 'missingstickers.html'
+})
+export class MissingStickersPage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
